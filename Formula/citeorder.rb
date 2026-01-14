d1acc1ac4b13e44cf90d12cc7ac73ee4099027f0
dhanushka2001 <dhanushka2001@gmail.com>
2026-01-14 03:58:49 +0000

    Revert "Update formula to citeorder 0.0.0-test6"
    This reverts commit 73df7b57653ae94ad17482ec14ea7d0de5cda0b8.

--- Formula/citeorder.rb
+++ Formula/citeorder.rb
@@ -1,8 +1,8 @@
 class Citeorder < Formula
   desc "Simple command-line tool to relabel footnotes in Markdown files in numerical order"
   homepage "https://github.com/dhanushka2001/citeorder"
-  url "https://github.com/dhanushka2001/citeorder/archive/refs/tags/v0.0.0-test6.tar.gz"
-  sha256 "f4a0303958020a79d4949e71fcbc02ad9013e39111d25ac51e1c7a6e55a62b33"
+  url "https://github.com/dhanushka2001/citeorder/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "772bd81c56020c9f7eab485bb01f821079eb393cacedd6afe55c999fc7de3494"
   license "GPL-3.0"
 
   def install
